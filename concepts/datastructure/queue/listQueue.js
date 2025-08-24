401c324f01125d2d29cee385bb020cd7be877b70
kw <kw@n.c>
2025-08-24 21:09:28 +0900

    update list queue

--- concepts/datastructure/queue/listQueue.js
+++ concepts/datastructure/queue/listQueue.js
@@ -32,3 +32,26 @@ console.log("isEmpty:", queue.length === 0); // true
 
 // 큐 크기 확인
 console.log("size:", queue.length); // 0
+
+idx = 0;
+
+// 데이터 넣기 (enqueue)
+queue.push(5);
+queue.push(4);
+queue.push(3);
+queue.push(2);
+queue.push(1);
+
+// 데이터 빼기 (dequeue)
+console.log("dequeue():", queue[idx++]); // 5
+console.log("dequeue():", queue[idx++]); // 4
+console.log("dequeue():", queue[idx++]); // 3
+console.log("dequeue():", queue[idx++]); // 2
+console.log("dequeue():", queue[idx++]); // 1
+console.log("dequeue():", queue[idx]); // undefined (더이상 없음)
+
+// 큐가 비었는지 확인
+console.log("isEmpty:", idx >= queue.length);
+
+// 큐 크기 확인
+console.log("size:", idx - queue.length); // 0
